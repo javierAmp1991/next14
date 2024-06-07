1ba056dbbb6481f7db3ed13786807bd5c6d5a754
javierAmp1991 <javier.ampuero.m91@hotmail.com>
2024-06-06 21:01:19 -0400

    commit 06-06-2024v7

--- packages/ui/src/default-card/default-card-mobile/default-card.tsx
+++ packages/ui/src/default-card/default-card-mobile/default-card.tsx
@@ -1,47 +1,41 @@
 //@ts-ignore
 import style from "./style.module.css";
-import {
-  CHECK_ICON_GREEN,
-  RIGHT_ARROW_ORANGE,
-  RIGHT_ARROW_BLACK,
-} from "../../icons";
+import {  RIGHT_ARROW_BLACK} from "../../icons";
 import Image from "next/image";
 import { IDefaultCard } from "../default-card";
+import Link from "next/link";
 
-export const DefaultCardMobile = ({
-  props}: {
-  props: IDefaultCard}) => {
-  const styleHover = props.NoHover
-    ? style.noHover
-    : props.Selected
-      ? style.selected
-      : style.noSelected;
-  return (
-    <div className={`${style.main}  ${styleHover} ${props.NewStyle}`}>
+export const DefaultCardMobile = ({props}: {props: IDefaultCard}) => {
+    const Children = () => <>
       <div className={style.bg} />
-      <div className={style.mainGrid}>
-        <div className={style.profile}>
-          <Image alt="" layout={"fill"} src={props.Image} />
-        </div>
-        <div className={style.contInformation}>
-          <div className={`${style.name}`}>{props.First}</div>
-          <div className={style.sku}>{props.Second}</div>
-        </div>
-        <div className={style.arrow}>
-          <Image
-            className={`${style.gray}`}
-            width={14}
-            height={14}
-            alt=""
-            src={RIGHT_ARROW_BLACK}
-          />
-        </div>
-      </div>
-      {props.Selected && (
-        <div className={style.icon}>
-          <Image alt="" layout={"fill"} src={CHECK_ICON_GREEN} />
-        </div>
-      )}
+  <div className={style.mainGrid}>
+    <div className={style.profile}>
+      <Image alt="" layout={"fill"} src={props.Image} />
+    </div>
+    <div className={style.contInformation}>
+      <div className={`${style.name}`}>{props.First}</div>
+      <div className={style.sku}>{props.Second}</div>
+    </div>
+    <div className={style.arrow}>
+      <Image
+        className={`${style.gray}`}
+        width={14}
+        height={14}
+        alt=""
+        src={RIGHT_ARROW_BLACK}
+      />
+    </div>
+  </div>
+    </>
+    const s = `${style.main} ${props.NewStyle}`;
+  return (
+    props.Href ?
+    <Link href={props.Href} className={s}>
+       <Children/>
+    </Link>
+    :
+    <div className={s}>
+       <Children/>
     </div>
   );
 };
