a2f68c346dd910b5f34551a5349b7c66747b06e9
javierAmp1991 <javier.ampuero.m91@hotmail.com>
2024-06-20 20:28:54 -0400

    commit 06-20-2024v8

--- packages/ui/src/misc/image-component/image-component.tsx
+++ packages/ui/src/misc/image-component/image-component.tsx
@@ -19,15 +19,16 @@ export const ImageComponent = ({props}:{props: IImageComponent}) => {
     const priority: boolean = props.Priority === undefined ? false : props.Priority;
     const alt: string = props.Alt || STRING_EMPTY;
     //const className: string = props.Use? mapeo[props.Use] : style.default;
-    const className: string = getStyle();
+    const nStyle: string = getStyle();
     return (
-        <div className={`${style.main} ${className} ${props.Style}`}>
+        <div className={`${style.main} ${nStyle} ${props.Style}`}>
             <Image alt={alt} priority={priority} layout={"fill"} objectFit={"cover"} src={props.Src}/>
         </div>
     )
 
     function getStyle(){
-        if(props.Use === EnumSizeImageComponent.use12) return style.use12
+        if(props.Use === undefined) return style.default
+        else if(props.Use === EnumSizeImageComponent.use12) return style.use12
         else if(props.Use === EnumSizeImageComponent.use14) return style.use14
         else if(props.Use === EnumSizeImageComponent.use16) return style.use16
         else return style.default
