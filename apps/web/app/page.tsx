98cb2be91d8a3a96af114881f832257594bcd52d
javierAmp1991 <javier.ampuero.m91@hotmail.com>
2024-07-22 18:03:57 -0400

    commit 22-07-2024v14

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,5 @@
 import styles from "./page.module.css";
 import Link from "next/link";
-import type { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: "Algo",
-  description: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-}
 
 export default function Page(): JSX.Element {
   return (
